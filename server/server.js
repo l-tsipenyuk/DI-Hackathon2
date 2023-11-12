04677f6fea18ed96403c6e3c76c783faf4b77da4
ltsipenyuk <lia.tsipenyuk@gmail.com>
2023-11-12 15:08:10 +0200

    server

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const quest_router = require("./routes/questions_routes.js");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/", express.static(__dirname + "/public"));
 app.use("/api", quest_router);
