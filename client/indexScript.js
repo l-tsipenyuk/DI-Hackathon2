2bc1c0888f48ff0b2fd7c07adf4a422123b45dd9
ltsipenyuk <lia.tsipenyuk@gmail.com>
2023-11-13 12:17:01 +0200

    email and readme

---
+++ client/indexScript.js
@@ -0,0 +1,29 @@
+const newsBtn = document.getElementById("newsBtn");
+const emailInput = document.getElementById("emailInput")
+
+newsBtn.addEventListener("click", async (event) => {
+    event.preventDefault();
+
+    const email = emailInput.value;
+
+    try {
+
+        const regResponse = await fetch(`http://localhost:3000/email/subscribe`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email }),
+        });
+
+        if (regResponse.ok) {
+            console.log("User subscribed.", regResponse);
+            newsBtn.textContent = "Now you subscribed."
+
+        } else {
+            console.error("User registration failed.");
+        }
+    } catch (err) {
+        console.error("An error occurred:", err);
+    }
+});
